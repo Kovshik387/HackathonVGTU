f284c5930bb52ed3d93f0d484436dd890c63eb70
mo0nchild <happytown77831@gmail.com>
2023-10-19 16:38:00 +0300

    fix: game over

--- HackathonVGTU/Controllers/TeacherController.cs
+++ HackathonVGTU/Controllers/TeacherController.cs
@@ -10,61 +10,22 @@ namespace HackathonVGTU.API.Controllers
         public ITeacherService TeacherService { get; private set; } = default!;
         public ILogger<TeacherController> Logger { get; private set; } = default!;
 
+        public TeacherController(ITeacherService teacherService, ILogger<TeacherController> logger) : base()
         {
-            var base_url = "https://cchgeu.ru";
-
-            var config = Configuration.Default.WithDefaultLoader();
-            using var context = BrowsingContext.New(config);
-
-            var url = "https://cchgeu.ru/studentu/nayti-kontakty-prepodavatelya/";
-
-            using var doc = await context.OpenAsync(url);
-
-            var pars = doc.QuerySelectorAll(@"p[class=""name""]");
-
-            List<TeacherEntity> teacher_data = new();
-
-            foreach ( var par in pars)
-        {
-                var doc_teacher = await context.OpenAsync(base_url + par.LastElementChild!.GetAttribute("href"));
-
-                var temp = doc.Title.Replace("\t","").Split(); // Имя фамилия
-
-                
-                
-                _logger.LogCritical(temp);
-
-
-
-                //teacher_data.Add(new TeacherEntity()
-                //{
-                //    Department
-                //});
+            this.TeacherService = teacherService;
+            this.Logger = logger;
         }
 
-            //foreach (var url_item in url_teacher ) 
-            //{
-            //    var doc_teacher = await context.OpenAsync(url_item);
-
-            //    var pars_teacher = doc_teacher.QuerySelector(@"div[class=""midle""]");
-
-            //}
-
-
-            //var url_teacher = 
-
-            //List<String> new_pars = new();
-
-            //foreach (var item in pars)
-            //{
-            //    _logger.LogWarning(item.TextContent.ToString());
-            //    var sub_item = item.TextContent.ToString().Replace("\t", "").Replace("\n", "");
-            //    new_pars.Add(sub_item);
-            //    _logger.LogInformation(sub_item.ToString());
-            //}
-
-            return teacher_data;
+        [HttpGet, Route("GetAllTeacher")]
+        public async Task<JsonResult> GetAllTeacher(string name)
+        {
+            return this.Json(await this.TeacherService.GetAllTeachers(name));
         }
 
+        [HttpGet, Route("GetTeacherByCode")]
+        public async Task<JsonResult> GetTeacherByCode(int code)
+        {
+            return this.Json(await this.TeacherService.GetTeacherByCode(code));
+        }
     }
 }
