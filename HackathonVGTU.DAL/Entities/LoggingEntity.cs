19611b34be436e9bafdd7e122d7b0b7a5c693fd1
mo0nchild <happytown77831@gmail.com>
2023-10-19 10:38:37 +0300

    feat: create ef database

---
+++ HackathonVGTU.DAL/Entities/LoggingEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonVGTU.DAL.Entities
+{
+    public partial class LoggingEntity : object
+    {
+        [KeyAttribute]
+        public int Id { get; set; } = default!;
+        public DateTime RequestTime { get; set; } = default!;
+
+        [MaxLength(20)]
+        public string UserIp { get; set; } = default!;
+
+        [MaxLength(100)]
+        public string CategoryName { get; set; } = default!;
+
+        [MaxLength(100)]
+        public string Value { get; set; } = default!;
+    }
+}
---
+++ HackathonVGTU.DAL/VgtuFinderDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonVGTU.DAL
+{
+    internal sealed class VgtuFinderDbContextFactory : IDesignTimeDbContextFactory<VgtuFinderDbContext>
+    {
+        public VgtuFinderDbContextFactory() : base() { }
+
+        public VgtuFinderDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<VgtuFinderDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Database=vgtucontest;Username=postgres;password=prolodgy778");
+
+            return new VgtuFinderDbContext(optionsBuilder.Options);
+        }
+    }
+}
