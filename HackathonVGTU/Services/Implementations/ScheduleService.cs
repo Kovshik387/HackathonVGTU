d6b8306148a01aa70c96d5ef393f88d7a844941e
mo0nchild <happytown77831@gmail.com>
2023-10-19 12:11:39 +0300

    feat: adding teacher's service

---
+++ HackathonVGTU/Services/Implementations/ScheduleService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using HackathonVGTU.API.Services.DataTransfer;
+using HackathonVGTU.API.Services.Interfaces;
+using HackathonVGTU.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackathonVGTU.API.Services.Implementations
+{
+    public partial class ScheduleService : IScheduleService
+    {
+        private readonly IDbContextFactory<VgtuFinderDbContext> factory = default!;
+
+        private readonly IMapper mapper = default!;
+        public ScheduleService(IDbContextFactory<VgtuFinderDbContext> factory, IMapper mapper) : base()
+        {
+            this.factory = factory;
+            this.mapper = mapper;
+        }
+        public async Task Add(ScheduleDto schedule)
+        {
+            using (var dbcontext = await this.factory.CreateDbContextAsync())
+            {
+
+            }
+        }
+
+        public async Task<List<string>> GetGroupsList()
+        {
+            return null;
+        }
+
+        public async Task<List<string>> GetGroupsListByName(string group)
+        {
+            return null;
+        }
+
+        public async Task<ScheduleDto?> GetSchedules(string group)
+        {
+            return null;
+        }
+    }
+}
---
+++ HackathonVGTU/Services/Interfaces/IScheduleService.cs
@@ -0,0 +1,13 @@
+﻿using HackathonVGTU.API.Services.DataTransfer;
+
+namespace HackathonVGTU.API.Services.Interfaces
+{
+    public interface IScheduleService
+    {
+        public Task Add(ScheduleDto schedule);
+
+        public Task<List<string>> GetGroupsListByName(string group);
+        public Task<List<string>> GetGroupsList();
+        public Task<ScheduleDto?> GetSchedules(string group);
+    }
+}
